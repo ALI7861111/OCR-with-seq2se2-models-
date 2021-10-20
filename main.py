2ffce167ce62bce51fbc6cb1b60f3793c8005110
ALI7861111 <57672011+ALI7861111@users.noreply.github.com>
2021-10-20 23:27:20 +0500

    wasteful comments removed

--- main.py
+++ main.py
@@ -30,14 +30,7 @@ epochs = args.epochs
 
 
 def objective(trial):
-    # CNN = NeuralNetwork(number_layers=3,input_channels=3,batch_size = 16,
-    #                 growth_factor=growth_array,num_layers_memory_unit=2,
-    #                 input_size=(1,3,50,200), hidden_size_memory_unit_1=128,
-    #                 hidden_size_memory_unit_2=64,linear_layer_units_1 = 512,
-    #                 linear_layer_units_2 = 256)
-    
-    #Sucess = False
-    #while Sucess == False: 
+
     nb_conv_layers = trial.suggest_categorical('number of convolution layers', [1,2,3])
     growth_array   = []
     for layer in range(0,nb_conv_layers):
@@ -85,23 +78,3 @@ def objective(trial):
 study = optuna.create_study(direction = 'minimize')
 study.optimize(objective, n_trials=100)
 
-#study.best_params  # E.g. {'x': 2.002108042}
-
-# trainer = train_NN(epochs=10,batch_size=16,directory_training_data=train_path,
-#                    directory_test_data=test_path,directory_val_data=valid_path)
-
-# growth_array = [1,2,2]
-
-# CNN = NeuralNetwork(number_layers=3,input_channels=3,batch_size = 16,
-#                     growth_factor=growth_array,num_layers_memory_unit=2,
-#                     input_size=(1,3,50,200), hidden_size_memory_unit_1=128,
-#                     hidden_size_memory_unit_2=64,linear_layer_units_1 = 512,
-#                     linear_layer_units_2 = 256, Unique_character_list = trainer.data_generator_train.Unique_character_list)
-
-# criterion = nn.CTCLoss(blank=5, reduction='mean', zero_infinity=True)
-# optimizer = torch.optim.Adam(CNN.parameters(), lr=0.001)
-
-
-
-
-# trainer.train(Neural_Network =CNN,criterion = criterion,learning_rate = 0.0001)
